1224f87b09a91c86209076e8cfdc2f9d7af57e4f
Guilherme Poppi <guilhermepoppilm@gmail.com>
2025-01-15 23:47:01 -0300

    subindo no repositorio para terminar mais tarde

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <header className='bg-white shadow-sm'>
+          <div className='max-w-5xl mx-auto px-4 py-3 flex items center'>
+
+            <h1 className='text-headerColor text-2xl font-sans font-medium'>Tradutor de Linguas</h1>
+
+          </div>
+        </header>
+
+      </div>
+    </>
+  )
+}
+
+export default App
